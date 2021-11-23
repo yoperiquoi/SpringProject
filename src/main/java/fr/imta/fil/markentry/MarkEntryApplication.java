6327e38fa07c6e6c1c382ab7b110b0700eee7ded
Yoann PERIQUOI <yoann_63115@hotmail.fr>
2021-11-23 12:12:29 +0100

    Initial commit of the project

---
+++ src/main/java/fr/imta/fil/markentry/MarkEntryApplication.java
@@ -0,0 +1,13 @@
+package fr.imta.fil.markentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkEntryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkEntryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/imta/fil/markentry/MarkEntryApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.imta.fil.markentry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkEntryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
